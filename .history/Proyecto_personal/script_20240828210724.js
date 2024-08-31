0894ff7a1c108ed2cf91ddd27575799b813e1c4f
TzStrikerYT <maicol.asus@gmail.com>
2024-08-30 20:56:11 -0500

    [add] add new row

---
+++ .history/Proyecto_personal/script_20240828210724.js
@@ -0,0 +1,21 @@
+for(let i = 1; i <= 10; i++){
+    if(i <= 9){
+      console.log(`${i},`)
+    } else {
+      console.log(i)
+    }
+  }
+
+let i = 1
+
+while(i <= 10){
+
+    // code here
+    if(i <= 9){
+        console.log(`${i},`)
+      } else {
+        console.log(i)
+      }
+    i++
+}
+  
---
+++ .history/Proyecto_personal/script_20240828211446.js
@@ -0,0 +1,40 @@
+for(let i = 1; i <= 10; i++){
+    if(i <= 9){
+      console.log(`${i},`)
+    } else {
+      console.log(i)
+    }
+  }
+
+let i = 1
+
+while(i <= 10){
+
+    // code here
+    if(i <= 9){
+        console.log(`${i},`)
+      } else {
+        console.log(i)
+      }
+    i++
+}
+
+for(let i = 1; i <= 3; i++){
+  
+    for(let j = 1; j <= 5; j++){
+      
+      if(i === 1){
+        console.log("a",j)
+      }
+      
+      if(i === 2){
+        console.log("b",j)
+      }
+      
+      if(i === 3){
+        console.log("c",j)
+      }
+    }
+    
+  }
+  
